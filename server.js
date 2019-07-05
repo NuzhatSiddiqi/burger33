45f1f0f219c618edfe938e167d6b4ee715f62f84
Nuzhat Siddiqi <46729206+NuzhatSiddiqi@users.noreply.github.com>
2019-07-05 13:08:27 -0400

    updating my project

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js")
+app.use(routes);
+
+
+app.listen(PORT, function () {
+  console.log("server listing on: http://localhost:" + PORT);
+});
+
+
+
+
+
+
+
+
