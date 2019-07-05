20817af3b857a5944ffdcc857d9a26d192b52ef0
Nuzhat Siddiqi <46729206+NuzhatSiddiqi@users.noreply.github.com>
2019-07-05 13:04:55 -0400

    project

---
+++ config/orm.js
@@ -0,0 +1,68 @@
+var connection = require("../config/connection");
+
+var orm = {
+    selectAll: function (table, cb) {
+        var dbQuery = "SELECT * FROM" + table + ";";
+
+        connection.query(dbQuery, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+    insertOne: function (table, clos, vals, cb) {
+
+        var dbQuery =
+            "INSERT INTO + table + 
+        "(" +
+            cols.toString() +
+            ") " +
+            "VALUES(" +
+            createQmarks(vals.lenth) +
+            ") ";
+
+        console.log(dbQuery);
+        connection.query(dbQuery), vals, function (err, res) {
+            if (err) {
+                throw err;
+            }
+
+            cb(res);
+        });
+
+    }
+    updateOne: function (table, objcolVals, condition, cb) {
+        var dbQuery =
+            "UPDATE " +
+            table +
+            " SET" +
+            translateSql(objcolVals) +
+            " WHERE " +
+            condition;
+
+        console.log(dbQuery);
+        connection.query(dbQuery), vals, function (err, res) {
+            if (err) {
+                throw err;
+            }
+
+            cb(res);
+        });
+    },
+    deleteOne: function (table, condition, cb) {
+        var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+        console.log(dbQuery);
+
+        connection.query(dbQuery), vals, function (err, res) {
+            if (err) {
+                throw err;
+            }
+
+            cb(res);
+        });
+
+    }
+
+};
+
